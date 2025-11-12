e1b73c8c1a3d45423b029df00e400c1e304610e2
Israel Hibraim <167723365+Hibra999@users.noreply.github.com>
2025-11-12 00:20:02 -0600

    Datos generados por yfinance

--- data/yfinance_data.py
+++ data/yfinance_data.py
@@ -1,16 +1,7 @@
 import yfinance as yf
 import pandas as pd
-import numpy as np
-from datetime import datetime
-import warnings
-warnings.filterwarnings("ignore")
-
-
-class extract_yf_data():
-    def __init__(self, start, end, accion):
-        self.start = start
-        self.end = end
-        self.accion = accion
-    def download(self, start, end, accion):
-        self.data = yf.download(self.accion, self.start, self.end)
-        return self.data
+import os
+os.makedirs('data', exist_ok=True)
+def download_yf(accion, start, end):
+    serie = yf.download(accion, start,  multi_level_index=False)
+    serie.to_csv(f"data/{accion}_2020-2025.csv")
--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import pandas as pd
 import numpy as np
 import warnings
+import os
+from data.yfinance_data import download_yf
 warnings.filterwarnings("ignore")
 
+#Extracción datos
+df = pd.DataFrame()                              
+tokens  = ['KO', 'AAPL', 'NVDA', 'JNJ', '^GSPC'] # acciones y el indice que escojimos, coca cola, apple, nvidia, jyj, s&p500
+start = "2019-12-31" # 2020
+end = "2025-11-01" # Hasta el 2025 del 31 de octubre
+
+for token in tokens:
+    df[token] = download_yf(token, start, end)
+
+df = pd.read_csv(r"C:\Users\hibra\Desktop\TT\data\AAPL_2020-2025.csv")
+print(df.head(20))
