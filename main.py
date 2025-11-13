a34297f72aa1d0f932f527a86cdd3cbada25985f
Israel Hibraim <167723365+Hibra999@users.noreply.github.com>
2025-11-13 00:27:07 -0600

    streamilit

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import warnings
 from data.yfinance_data import download_yf
 from data.ccxt_data import download_cx
 warnings.filterwarnings("ignore")
+import streamlit as st #Interfaz para facilitarnos el trabajo
 
 #Extracción datos
 df = pd.DataFrame()    
-
 start = "2019-12-31" # 2020
 end = "2025-11-01" # Hasta el 2025 del 31 de octubre
 
@@ -19,5 +19,6 @@ cryptos = ["BTC/USDT", "ETH/USDT"] # bitcoin y etherium
 download_cx(cryptos, start, end)
 
 #df = pd.read_csv(r"C:\Users\hibra\Desktop\TT\data\AAPL_2020-2025.csv")
-df = pd.read_csv(r"")
-print(df.head(20))
+st.title('TT')
+df = pd.read_csv(r"C:\Users\hibra\Desktop\TT\data\tokens\AAPL_2020-2025.csv") #Hay que mejorar esto del directorio cesarin
+st.dataframe(df.head(20))
