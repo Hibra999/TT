d901ad3c0fa8a550212f0328f36d603dc197fc03
Hibra999 <miarsito1@gmail.com>
2025-11-20 00:17:46 -0600

    todos mismo shape

--- data/yfinance_data.py
+++ data/yfinance_data.py
@@ -7,4 +7,6 @@ os.makedirs('data/tokens', exist_ok=True)
 def download_yf(acciones, start, end):
     for accion in acciones:
         serie = yf.download(accion, start, end, multi_level_index=False)
+        serie["Date_final"] = pd.to_datetime(serie.index)
+        serie["Date_final"] = serie["Date_final"].dt.date
         serie.to_csv(f"data/tokens/{accion}_2020-2025.csv")
--- features/macroeconomics.py
+++ features/macroeconomics.py
@@ -7,9 +7,13 @@ warnings.filterwarnings("ignore")
 start = datetime(2020,1,1)
 end = datetime(2025, 10, 31)
 
-def macroeconomicos():
+def macroeconomicos(tiempo):
     macro = web.DataReader(["GDP","CPIAUCSL","FEDFUNDS","DGS10","SOFR","UNRATE"],"fred", start=start, end=end)
-    columnas = macro.columns
-    for col in columnas:
-        macro[col] = macro[col].ffill() # ya quedo el ffill
-    return macro.dropna()
+    macro = macro.reset_index()
+    macro["Date"] = pd.to_datetime(macro["DATE"]).dt.date
+    macro = macro.drop("DATE", axis=1)
+    macro = macro.ffill()
+    macro = macro[macro["Date"].isin(pd.to_datetime(tiempo).dt.date)]
+    macro = macro.dropna()
+    macro = macro.set_index("Date")
+    return macro
