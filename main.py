ec61414a1d96966f8d24676db25b01b38f18a7dc
Israel Hibraim <167723365+Hibra999@users.noreply.github.com>
2025-11-20 02:08:23 -0600

    MICFS + nueva interfaz

--- main.py
+++ main.py
@@ -1,48 +1,68 @@
 import pandas as pd
+import numpy as np
+import seaborn as sns
 import warnings
+import streamlit as st
 from data.yfinance_data import download_yf
 from data.ccxt_data import download_cx
 from features.macroeconomics import macroeconomicos
 from features.tecnical_indicators import TA
 from features.top_n import top_k
-import streamlit as st
+
 warnings.filterwarnings("ignore")
+st.set_page_config(layout="wide")
+
+with st.sidebar:
+    start = "2020-01-01"
+    end = "2025-11-01"
+
+    @st.cache_data
+    def load_data():
+        tokens = ['KO', 'AAPL', 'NVDA', 'JNJ', '^GSPC', "GC=F", "CBOE"]
+        dy = download_yf(tokens, start, end)
+        cryptos = ["BTC/USDT", "ETH/USDT"]
+        dc = download_cx(cryptos, start, end)
+        return dy, dc
+
+    load_data()
+    token = st.selectbox(label="ACTIVO FINANCIERO: ", options=['KO', 'AAPL', 'NVDA', 'JNJ', '^GSPC', "BTC-USDT", "ETH-USDT"])
 
 st.title('TT')
-#Extracción datos
-df = pd.DataFrame()    
-start = "2020-01-01" # 2020
-end = "2025-11-01" # Hasta el 2025 del 31 de octubre
 
-#Criptomonedas
-@st.cache_data
-def load_data():
-    #Acciones y indices
-    tokens  = ['KO', 'AAPL', 'NVDA', 'JNJ', '^GSPC', "GC=F", "CBOE"] 
-    dy = download_yf(tokens, start, end)
-    cryptos = ["BTC/USDT", "ETH/USDT"] # bitcoin y etherium
-    dc = download_cx(cryptos, start, end)
-    return dy, dc
-load_data()
+df = pd.read_csv(rf"C:\Users\hibra\Desktop\TT\data\tokens\{token}_2020-2025.csv")
 
-token = st.selectbox(label="ACTIVO FINANCIERO: ", options=['KO', 'AAPL', 'NVDA', 'JNJ', '^GSPC', "BTC-USDT", "ETH-USDT"])
-df = pd.read_csv(rf"C:\Users\hibra\Desktop\TT\data\tokens\{token}_2020-2025.csv") #Hay que mejorar esto del directorio cesarin
-st.dataframe(df.head(31))
+tab1, tab2, tab3 = st.tabs(["Datos & Retornos", "Indicadores (TA/Macro)", "Modelo (MIC)"])
 
-st.subheader("Indicadores Tecnicos")
-df_ta = TA(df)
-st.dataframe(df_ta.head(31))
+with tab1:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("CLOSE")
+        st.line_chart(df["Close"])
+        st.dataframe(df.head(31))
+    with col2:
+        st.subheader("LOG RETURN")
+        log_df = np.log(df["Close"] / df["Close"].shift(-1)).dropna()
+        st.line_chart(log_df)
 
-st.subheader("Datos Macroeconomicos")
-df_ma = macroeconomicos(df["Date_final"])
-st.dataframe(df_ma.head(31))
+with tab2:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.subheader("Indicadores Tecnicos")
+        df_ta = TA(df)
+        st.dataframe(df_ta.head(31))
+    with col2:
+        st.subheader("Datos Macroeconomicos")
+        df_ma = macroeconomicos(df["Date_final"])
+        st.dataframe(df_ma.head(31))
 
-df_ta = df_ta.reset_index(drop=True)
-df_ma = df_ma.reset_index(drop=True)
+with tab3:
+    df_ta = df_ta.reset_index(drop=True)
+    df_ma = df_ma.reset_index(drop=True)
+    
+    st.subheader("DF_final")
+    df_final = pd.concat([df_ta, df_ma], axis=1)
+    st.dataframe(df_final)
 
-st.subheader("DF_final")
-df_final = pd.concat([df_ta, df_ma], axis=1)
-st.dataframe(df_final)
-
-st.subheader("MIC: top n caracteristicas")
-features = top_k()
+    st.subheader("MIC: top n caracteristicas")
+    features = top_k(df_final.dropna(axis=0), log_df, 15)
+    st.write(features)
