808e630b8660dd1619a5b7928a0522e21926876e
Hibra999 <miarsito1@gmail.com>
2025-11-21 02:23:25 -0600

    correcion

--- preprocessing/walk_forward.py
+++ preprocessing/walk_forward.py
@@ -4,10 +4,10 @@ from sktime.forecasting.model_selection import SlidingWindowSplitter
 def wfrw(y_data, k=5, fh_val=30):
     n_samples = len(y_data)
     fh = np.arange(1, fh_val + 1)
-    window_length = int(n_samples * 0.5)
-    usable_samples = n_samples - window_length - fh_val
-    step_length = max(1, int(usable_samples // (k - 1)))
-    splitter = SlidingWindowSplitter(fh=fh, window_length=window_length, step_length=step_length)
+    ventana = int(n_samples * 0.5)
+    oof = n_samples - ventana - fh_val
+    pasos = max(1, int(oof // (k - 1)))
+    splitter = SlidingWindowSplitter(fh=fh, window_length=ventana, step_length=pasos)
     print(f"folds: {splitter.get_n_splits(y_data)}")
     return splitter
     
