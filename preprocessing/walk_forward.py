040afd2cffdac4fb6298bf787a58a686df19b6b1
CAEZARVS2001 <donselley0105@gmail.com>
2026-03-24 16:57:38 -0600

    fix: MOdified window length to be compatible with actives of yf

--- preprocessing/walk_forward.py
+++ preprocessing/walk_forward.py
@@ -14,6 +14,12 @@ def wfrw(y_data, k=5, fh_val=30, window_ratio=0.5):
     n_samples = len(y_data)
     fh = np.arange(1, fh_val + 1)
     ventana = int(n_samples * window_ratio)
+    # Asegurar que window_length + fh_val < n_samples (requisito de sktime)
+    max_ventana = n_samples - fh_val - 1
+    if ventana > max_ventana:
+        print(f"  [WARN] window_length={ventana} + fh={fh_val} >= n_samples={n_samples}. "
+              f"Reduciendo window_length a {max_ventana}")
+        ventana = max_ventana
     oof = n_samples - ventana - fh_val
     pasos = max(1, int(oof // (k - 1)))
     splitter = SlidingWindowSplitter(fh=fh, window_length=ventana, step_length=pasos)
